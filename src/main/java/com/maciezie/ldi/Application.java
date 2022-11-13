41a51a577b1762458c72ab2d3b2555f714baf898
Maciej Zieliński <maciej.zielinski@idemia.com>
2022-11-13 15:05:33 +0100

    Add open-api

--- src/main/java/com/maciezie/ldi/Application.java
+++ src/main/java/com/maciezie/ldi/Application.java
@@ -1,7 +1,15 @@
 package com.maciezie.ldi;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "flights",
+                version = "0.0"
+        )
+)
 public class Application {
 
     public static void main(String[] args) {
